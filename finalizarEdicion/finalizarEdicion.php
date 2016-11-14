793a9afced3ebb2693c472279be3dbfcf8dcaf4f
Julian da Costa Faro <juliandacostafaro@hotmail.com>
2016-11-14 13:55:17 -0300

    App de finalizacion comparo por el usuario que se logueo en google.

--- finalizarEdicion/finalizarEdicion.php
+++ finalizarEdicion/finalizarEdicion.php
@@ -18,10 +18,19 @@ if (!isset($_GET['code'])) {
 	$_SESSION['access_token'] = $client->getAccessToken();
 	session_start();
 	$id_autor=$_SESSION['id'];
-	
+	$drive_service = new Google_Service_Drive($client);
+        $optParams = array(  
+         'fields' => 'user(emailAddress)'
+         );
+        $mail = $drive_service->about->get($optParams);
+        $emailPersonal = $mail->toSimpleObject()->user['emailAddress'];
 	//sacar funcion md5
-	$autor=get_autor_md5(md5($id_autor));
-
-	load_template_twig("finalizarVista.html", array('autor' => $autor));   
+	//$autor=get_autor_md5(md5($id_autor));
+	$autor=get_autor_md5($id_autor);
+	$mismoUsuario=true;
+	if ($autor["CORREO_GMAIL"] != $emailPersonal){
+		$mismoUsuario=false;
+	}
+	load_template_twig("finalizarVista.html", array('autor' => $autor ,'mismoUsuario' => $mismoUsuario));   
 }
 ?>
