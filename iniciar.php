14b305ca521a4bf61e8621d44161b0b857abc235
Julian da Costa Faro <juliandacostafaro@hotmail.com>
2016-11-21 17:23:52 -0300

    iniciar.php para pedir credenciales

--- iniciar.php
+++ iniciar.php
@@ -72,11 +72,14 @@ function expandHomeDirectory($path) {
   return str_replace('~', realpath($homeDirectory), $path);
 }
 // Get the API client and construct the service object.
+
+
+
 $client = getClient();
 $service = new Google_Service_Drive($client);
 
 // Print the names and IDs for up to 10 files.
-$optParams = array(
+/*$optParams = array(
   'pageSize' => 10,
   'fields' => 'nextPageToken, files(id, name)'
 );
@@ -89,4 +92,4 @@ if (count($results->getFiles()) == 0) {
   foreach ($results->getFiles() as $file) {
     printf("%s (%s)\n", $file->getName(), $file->getId());
   }
-}
+}*/
